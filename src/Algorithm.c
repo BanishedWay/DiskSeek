58661ade72bc6dd84546da98a42cdcad525ff377
JinqiXu <xujinqi2001@outlook.com>
2021-06-05 18:16:19 +0800

    添加cscan和fscan算法

--- src/Algorithm.c
+++ src/Algorithm.c
@@ -87,25 +87,132 @@ void SCAN(int array_Track[], int m)
     float sum = 0, avg = 0;
     printf("请输入当前磁道号：\n");
     scanf("%d", &cur);
-    for (i = 0; i < m;i++)
+    for (i = 0; i < m; i++)
     {
         temp[i] = array_Track[i];
     }
     sort(temp, m);
-    while(temp[k]<cur)
+    while (temp[k] < cur)
     {
         k++;
     }
     l = k - 1;
     r = k;
-    printf("请输入移动臂移动方向：\n1：大于当前磁道号\n2：小于当前磁道号\n");
+    printf("请输入移动臂移动方向：\n1：磁道号增加方向\n2：磁道号减小方向\n");
     scanf("%d", &count);
-    
+    if (count == 1)
+    {
+        printf("磁道访问序列：\n");
+        for (j = l; j >= 0; j--)
+        {
+            printf("%d ", temp[j]);
+        }
+        for (int j = r; j < m; j++)
+        {
+            printf("%d ", temp[j]);
+        }
+        sum = cur - 2 * temp[0] + temp[m - 1];
+    }
+    else
+    {
+        printf("磁道访问序列：\n");
+        for (int j = r; j < m; j++)
+        {
+            printf("%d ", temp[j]);
+        }
+        for (j = l; j >= 0; j--)
+        {
+            printf("%d ", temp[j]);
+        }
+        sum = cur - temp[0] + 2 * temp[m - 1];
+    }
+    avg = sum / m;
+    printf("\n平均寻道长度：%.3f\n", avg);
 }
 
 void CSCAN(int array_Track[], int m)
 {
+    int i, j, k = 0, r = 0, l = 0, cur;
+    float sum = 0, avg = 0;
+    int temp[MaxTrack];
+    for (i = 0; i < m; i++)
+    {
+        temp[i] = array_Track[i];
+    }
+    sort(temp, m);
+    printf("请输入当前磁道号：\n");
+    scanf("%d", &cur);
+    printf("磁道访问序列：\n");
+    while (temp[k] < cur)
+    {
+        k++;
+    }
+    l = k - 1;
+    r = k;
+    for (j = r; j < m; j++)
+    {
+        printf("%d ", temp[j]);
+    }
+    for (j = 0; j < r; j++)
+    {
+        printf("%d ", temp[j]);
+    }
+
+    sum = 2 * temp[m - 1] + temp[l] - cur - 2 * temp[0];
+    avg = sum / m;
+    printf("\n平均寻道长度：%.3f\n", avg);
 }
 void FSCAN(int array_Track[], int m)
 {
+    int i, j, k, cur;
+    float sum, avg;
+    int temp1[MaxTrack], temp2[MaxTrack];
+    for (i = 0; i < m / 2; i++)
+        temp1[i] = array_Track[i];
+    for (i = m / 2; i < m; i++)
+        temp2[i - (m / 2)] = array_Track[i];
+    sort(temp1, m / 2);
+    printf("请输入第一段磁道初始地址：\n");
+    scanf("%d", &cur);
+    k = 0;
+    while (temp1[k] < cur)
+    {
+        k++;
+    }
+    sum += abs(temp1[k] - cur);
+    printf("磁道访问序列：\n");
+    printf("%d ", temp1[k]);
+    for (j = k; j < (m / 2) - 1; j++)
+    {
+        sum += abs(temp1[j + 1] - temp1[j]);
+        printf("%d ", temp1[j + 1]);
+    }
+    for (j = 0; j < k - 1; j++)
+    {
+        sum += abs(temp1[j + 1] - temp1[j]);
+        printf("%d ", temp1[j + 1]);
+    }
+    printf("\n");
+    sort(temp2, m / 2);
+    printf("请输入第二段磁道初始地址：\n");
+    scanf("%d", &cur);
+    k = 0;
+    while (temp2[k] < cur)
+    {
+        k++;
+    }
+    sum += abs(temp2[k] - cur);
+    printf("磁道访问序列：\n%d ", temp2[k]);
+    for (j = k; j < m / 2 - 1; j++)
+    {
+        sum += abs(temp2[j + 1] - temp2[j]);
+        printf("%d ", temp2[j + 1]);
+    }
+    for (j = 0; j < k; j++)
+    {
+        sum += abs(temp2[j + 1] - temp2[j]);
+        printf("%d ", temp2[j]);
+    }
+    avg = sum / m;
+    printf("\n平均寻道长度：%.3f\n", avg);
 }
